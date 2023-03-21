31815d0468924f51932d4075a97a8b32851f37b8
JG24 <marek@marusz.net>
2023-03-21 20:22:33 +0100

    Update SimplePinger.sh

--- SimplePinger.sh
+++ SimplePinger.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 # Target IP
-CUSTOM_IP=dns.google
+CUSTOM_IP="dns.google"
 
 # Log file
 LOG_FILE=network_log.csv
 
 # Delay
-DELAY=5
-
-RESULT=`/usr/bin/ping -w 1 -c 1 $CUSTOM_IP > /dev/null && echo "PING_OK"`
+DELAY=7
 
 echo "Time,IP,Status" >> $LOG_FILE
 while true; do
 	TIMESTAMP=$(date +"%Y-%m-%d %T")
+	RESULT=`/usr/bin/ping -w 2 -c 1 $CUSTOM_IP > /dev/null && echo "PING_OK"`
 	
 	if [ "$RESULT" = "PING_OK" ]; then
 		echo "$TIMESTAMP: Network is OK"
